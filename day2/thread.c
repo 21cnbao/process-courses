9baaffa9e2b043ea76abe241d23efe56ad94fb7e
Barry Song <21cnbao@gmail.com>
2017-09-15 19:42:49 +0800

    create two threads
    Signed-off-by: Barry Song <21cnbao@gmail.com>

--- day2/thread.c
+++ day2/thread.c
@@ -12,23 +12,35 @@ static pid_t gettid( void )
 static void *thread_fun(void *param)
 {
 	printf("thread pid:%d, tid:%d pthread_self:%lu\n", getpid(), gettid(),pthread_self());
+	while(1);
 	return NULL;
 }
 
 int main(void)
 {
-	pthread_t tid;
+	pthread_t tid1, tid2;
 	int ret;
 
 	printf("thread pid:%d, tid:%d pthread_self:%lu\n", getpid(), gettid(),pthread_self());
 
-	ret = pthread_create(&tid, NULL, thread_fun, NULL);
+	ret = pthread_create(&tid1, NULL, thread_fun, NULL);
 	if (ret == -1) {
 		perror("cannot create new thread");
 		return -1;
 	}
 
-	if (pthread_join(tid, NULL) != 0) {
+	ret = pthread_create(&tid2, NULL, thread_fun, NULL);
+	if (ret == -1) {
+		perror("cannot create new thread");
+		return -1;
+	}
+
+	if (pthread_join(tid1, NULL) != 0) {
+		perror("call pthread_join function fail");
+		return -1;
+	}
+
+	if (pthread_join(tid2, NULL) != 0) {
 		perror("call pthread_join function fail");
 		return -1;
 	}
